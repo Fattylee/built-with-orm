53a6ae6384c8d125f354e77c64920d04b8521f00
Fatai Balogun <fattylee.remod@gmail.com>
2019-12-18 07:59:14 +0100

    save a single page app

---
+++ client/index.js
@@ -0,0 +1,29 @@
+/* eslint-disable react/react-in-jsx-scope  */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-undef */
+
+const jsx = (
+  <div>
+    <h1>Hello React</h1>
+    <hr />
+    <ul>
+      <li>
+        <a href="facebook">Facebook</a>
+      </li>
+      <li>
+        <a href="google">Google</a>
+      </li>
+      <li>
+        <a href="twitter">Twitter</a>
+      </li>
+      <li>
+        <a href="instagram">Instagram</a>
+      </li>
+    </ul>
+    <h2>Walhamdulllilaahi rabi l alamin</h2>
+    <h3>This is becoming interesring...</h3>
+    <hr />
+    <p>this paragraph is awesome!</p>
+  </div>
+);
+ReactDOM.render(jsx, document.querySelector('#fake'));
