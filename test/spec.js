f8531f99dbe1a122ed7e6cd273e33ea01dd36ce6
Fatai Balogun <fattylee.remod@gmail.com>
2020-01-01 21:51:49 +0100

    again another attempt as above

--- test/spec.js
+++ test/spec.js
@@ -37,43 +37,75 @@ describe('my app', () => {
     });
     it('should succeed on a . den', () =>
       request
-        .get('/foo')
-        .expect(200)
-        .then(res => expect(res.body.data).toBe('foo bar')));
+      .get('/foo')
+      .expect(200)
+      .then(res => expect(res.body.data).toBe('foo bar')));
     it('should bar from foo body', () =>
       request
-        .post('/foo')
-        .send({ foo: 'bar' })
-        .expect(201)
-        .then(res => expect(res.body.data).toBe('BAR')));
+      .post('/foo')
+      .send({ foo: 'bar' })
+      .expect(201)
+      .then(res => expect(res.body.data).toBe('BAR')));
     const ggghh = 677;
     console.log(ggghh, 56);
   });
   describe('Get head', () => {
     it('set header', () =>
       request
-        .get('/head')
-        .set('abu', 'lulu')
-        .expect(200)
-        .then(res => {
-          expect(res.text).toBe('lulu');
-        }));
+      .get('/head')
+      .set('abu', 'lulu')
+      .expect(200)
+      .then(res => {
+        expect(res.text).toBe('lulu');
+      }));
   });
   describe('Get api/v1', () => {
     it('get all categories', () =>
       request
-        .get('/api/v1/categories')
-        .expect(200)
-        .then(res => {
-          expect(res.body.some(c => c.name === 'category a')).toBeTruthy();
-          expect(res.body.some(c => c.name === 'category f')).toBeFalsy();
-        }));
+      .get('/api/v1/categories')
+      .expect(200)
+      .then(res => {
+        expect(res.body.some(c => c.name === 'category a')).toBeTruthy();
+        expect(res.body.some(c => c.name === 'category f')).toBeFalsy();
+      }));
     it('get all products', () =>
       request
-        .get('/api/v1/products')
-        .expect(200)
-        .then(res => {
-          expect(res.body.length).toBe(7);
-        }));
+      .get('/api/v1/products')
+      .expect(200)
+      .then(res => {
+        expect(res.body.length).toBe(7);
+      }));
+  });
+  it('freestyle', () => {
+    const obj = 'abu is assiduos';
+    const arr = [2, 'ff', true];
+    const o = { id: 1, name: 'gth' };
+    const args = [null, undefined, '', 0, NaN, false];
+    const throwEx = (input) => {
+      if (!input) throw new Error('dabaru');
+    };
+    expect(obj).toMatch('abu');
+    expect(obj).toMatch(/IS/i);
+    expect(obj).toContain('is');
+    expect(arr).toEqual(expect.arrayContaining([2, true]));
+    expect(arr).toContain(2);
+    expect(o).toEqual({ id: 1, name: 'gth' });
+    expect(o).toHaveProperty('id', 1);
+    expect(o).toMatchObject({ name: 'gth' });
+    expect(o).toStrictEqual({ id: 1, name: 'gth' });
+    args.forEach((a) =>
+      expect(() => {
+      throwEx(a);
+    }).toThrow(/dA/i)
+    );
+  });
+  describe('FIZZBUZZ', () => {
+    const getFizzbuzz = (num) => {
+      if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+    };
+    it('should return a fizzbuzz', () => {
+      const result = getFizzbuzz(30);
+      console.log(expect(result).toMatch('izzbuzz'));
+    });
   });
 });
