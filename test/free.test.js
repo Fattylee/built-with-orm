f7804b4c1b1165c3e71e85276b4c21406211de36
Fatai Balogun <fattylee.remod@gmail.com>
2020-01-01 21:47:18 +0100

    save with prettier-eslint husky and lint-staged

---
+++ test/free.test.js
@@ -0,0 +1,11 @@
+/*
+ *
+ * create .editorconfig and define all styles
+ * install eslint, at this point u can lint file but no go formatting yet
+ * install prettier-eslint for a nice formatting
+ * tweak ur text-editor for nice formatting
+ *  */
+
+console.log('456', '32', 'thhj');
+const bob = '56';
+console.log(bob);
