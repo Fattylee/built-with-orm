b6302ffcb029acb7918ef931856cc10091b95015
Fatai Balogun <fattylee.remod@gmail.com>
2020-01-30 07:14:53 +0100

    enable huskyrc, fix eslint errors

--- test/seed/syncAndSeed.test.js
+++ test/seed/syncAndSeed.test.js
@@ -2,7 +2,7 @@ import syncAndSeed from '../../server/src/seed/syncAndSeed';
 
 describe('syncAndSeed', () => {
   it('should throw error upon syncAndSeed method call', () => {
-    const res = syncAndSeed();
-    console.log(res);
+    syncAndSeed();
+    // console.log(res);
   });
 });
